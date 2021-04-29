0e7f154f0cc1bfeff7f6bb9ca524f564c66a887c
yarin bnyamin <bnyamin@post.bgu.ac.il>
2021-04-29 18:38:32 +0300

    working simple compressing

---
+++ src/IO/SimpleCompressorOutputStream.java
@@ -0,0 +1,43 @@
+package IO;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class SimpleCompressorOutputStream extends MyCompressorOutputStream {
+
+    public SimpleCompressorOutputStream(OutputStream out) {
+        super(out);
+    }
+
+    @Override
+    public void write(int b) throws IOException {
+        out.write(b);
+    }
+
+
+    @Override
+    public void write(byte[] b) throws IOException {
+
+        for (int i = 0; i < 12; i++)
+            write(Byte.toUnsignedInt(b[i]));
+
+        byte lastNum = 0;
+        int sum = 0;
+        for (int i = 12; i < b.length; i++) {
+            if(lastNum == b[i]){
+                if(sum < 255) {
+                    sum++;
+                }else{
+                    write(sum);
+                    sum = 1;
+                }
+            }else{
+                write(sum);
+                sum = 1;
+                lastNum = b[i];
+            }
+        }
+        write(sum);
+
+    }
+}
---
+++ src/IO/SimpleDecompressorInputStream.java
@@ -0,0 +1,49 @@
+package IO;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+public class SimpleDecompressorInputStream extends MyDecompressorInputStream {
+
+    public SimpleDecompressorInputStream(InputStream in) {
+        super(in);
+    }
+
+    @Override
+    public int read() throws IOException {
+        return in.read();
+    }
+
+    @Override
+    public int read(byte[] b) throws IOException {
+
+        for (int i = 0; i < 12; i++)
+            b[i] = (byte) read();
+
+        int pos = 12;
+        byte num = 0;
+        int count;
+        for (int i = 12; i < b.length; i++) {
+            count = read();
+            /*
+            if(count == 0) {
+                i -= 2;
+                continue;
+            }
+            */
+
+            for (int j = 0; j < count; j++) {
+                b[pos] = num;
+                pos++;
+            }
+
+            if(num == 1)
+                num = 0;
+            else
+                num = 1;
+        }
+
+        return 0;
+    }
+}
