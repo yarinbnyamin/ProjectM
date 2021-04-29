a8c72d986b09b39fef5095ef3294a199f9aa6312
Eldar <eldark@post.bgu.ac.il>
2021-04-29 16:33:45 +0300

    Maze methods for part B

--- src/algorithms/mazeGenerators/Maze.java
+++ src/algorithms/mazeGenerators/Maze.java
@@ -19,6 +19,23 @@ public class Maze {
         this.end_position = new Position(rows-1,columns-1);
     }
 
+    public Maze(byte[] info) {
+        this.rows = info[0]*256 + info[1] ;
+        this.columns = info[2]*256 +info[3];
+        this.maze = new int[rows][columns];
+        Position startPos = new Position(info[4] * 256 + info[5], info[6] * 256 + info[7]);
+        Position GoalPos = new Position(info[8] * 256 +info[9], info[10] * 256 + info[11]);
+        setStartPosition(startPos);
+        setGoalPosition(GoalPos);
+        int CurrentCell = 12;
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < columns; col++) {
+                maze[rows][columns]=info[CurrentCell];
+                CurrentCell++;
+            }
+        }
+    }
+
     public void print(){
         boolean found_start= false;
         boolean found_goal= false;
@@ -59,6 +76,31 @@ public class Maze {
         // if the point is in the maze
         return c >= 0 && c < columns && r >= 0 && r < rows;
     }
+
+    public byte[] toByteArray() {
+        byte[] byteMazeInfo = new byte[12 + rows * columns];
+        byteMazeInfo[0] = (byte)(this.rows / 256);
+        byteMazeInfo[1] = (byte)(this.rows % 256);
+        byteMazeInfo[2] = (byte)(this.columns / 256);
+        byteMazeInfo[3] = (byte)(this.columns % 256);
+        byteMazeInfo[4] = (byte)(this.start_position.getRowIndex() / 256);
+        byteMazeInfo[5] = (byte)(this.start_position.getRowIndex() % 256);
+        byteMazeInfo[6] = (byte)(this.start_position.getColumnIndex() / 256);
+        byteMazeInfo[7] = (byte)(this.start_position.getColumnIndex() % 256);
+        byteMazeInfo[8] = (byte)(this.end_position.getRowIndex() / 256);
+        byteMazeInfo[9] = (byte)(this.end_position.getRowIndex() % 256);
+        byteMazeInfo[10] = (byte)(this.end_position.getColumnIndex() / 256);
+        byteMazeInfo[11] = (byte)(this.end_position.getColumnIndex() % 256);
+        int counter = 12;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                byteMazeInfo[counter] = (byte)getMaze()[i][j];
+                counter++;
+            }
+        }
+        return byteMazeInfo;
+    }
+
 
     public int[][] getMaze() {
         return maze;
